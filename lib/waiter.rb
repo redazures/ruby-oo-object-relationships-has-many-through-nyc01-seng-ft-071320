ade9da3df0b8428a394d0807a82dc797c5f30324
redazures <jwu_nyc@yahoo.com>
2020-07-17 08:35:56 -0400

    added worst tipper

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -30,4 +30,12 @@ class Waiter
         best_tipped_meal.customer
     end
 
+    def worst_tipper
+        best_tipped_meal = meals.min do |meal_a, meal_b|
+          meal_a.tip <=> meal_b.tip
+        end
+       
+        best_tipped_meal.customer
+    end
+
 end
